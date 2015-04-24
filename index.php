4a1bec706a2f13a1bd5a2f3ff92c865628fbdecc
donaji13 <toltekochoa12@gmail.com>
2015-04-23 19:53:12 -0700

    finished video 9

--- index.php
+++ index.php
@@ -13,4 +13,24 @@
 <a class="btn btn-primary" href="register.php">Register</a>
 
 <div class="wrap">
-	
+	<div class="task-list">
+	<ul>
+	<?php require("includes/connect.php"); 
+	$mysqli = new mysqli('localhost', 'root', 'root', 'todo2');
+	$query = "SELECT * FROM tasks ORDER BY date ASC, time ASC";
+	if ($result = $mysqli->query($query)) {
+		$numrows = $result->num_rows;
+		if ($numrows>0) {
+			while($row = $result->fetch_assoc()){
+				$task_id = $row['id'];
+				$task_name = $row["task"];
+				echo '<li>
+				<span>'.$task_name.'</span>
+                <img id="'.$task_id.'"" class="delete-button" width="10px" src="images/close.svg"/>
+				</li>';
+			}
+		}
+	}
+	?>
+	</ul>
+</html>
