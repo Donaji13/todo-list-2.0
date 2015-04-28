e5a68cf23af8251a1421387df792e443cf13d07d
donaji13 <toltekochoa12@gmail.com>
2015-04-27 20:48:49 -0700

    fixed alot of errors with login and it works

--- includes/connect.php
+++ includes/connect.php
@@ -1,7 +1,7 @@
 <?php
 //why is this important? Why does localhost have to be first?
 //this is localhost, user name, password, and the database. They have to be in order for the database to even exits
-$mysqli = new mysqli("localhost","root", "root", "todo2");
+$mysqli = new mysqli("localhost","root", "root", "todo");
 //$mysqli->connect_error then we want it to die and have this message
 if($mysqli->connect_error){
 die('Connect Error(' . $mysqli->connect_errno .')'
